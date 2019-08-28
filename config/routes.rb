1d3ac62db0c82f5867628a30d7903291508ec944
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-08-28 17:30:02 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   
-  resources :posts, only: [:index, :show, :new, :create]
+  resources :coupons, only: [:index, :show, :new, :create]
 end
